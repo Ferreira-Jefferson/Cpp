3379d8ec70a84089589e3b9925e1e21aa279e918
Ferreira-Jefferson <jefferson.ctc.ferreira@outlook.com>
2025-12-31 14:32:54 -0300

    refactor: module 2 - ex02

--- module 02/ex02/Fixed.cpp
+++ module 02/ex02/Fixed.cpp
@@ -41,28 +41,20 @@ int Fixed::toInt(void) const {
 	return (_valueFixedPoint >> _bits);
 }
 
-Fixed& Fixed::min(Fixed& a, Fixed&b) {
-	if (a.toFloat() < b.toFloat())
-		return (a);
-	return (b);
+Fixed& Fixed::min(Fixed& a, Fixed& b) {
+	return (a < b ? a : b);
 }
 
 const Fixed& Fixed::min(const Fixed& a, const Fixed& b) {
-	if (a.toFloat() < b.toFloat())
-		return (a);
-	return (b);
+	return (a < b ? a : b);
 }
 
-Fixed& Fixed::max(Fixed& a, Fixed&b) {
-	if (a.toFloat() > b.toFloat())
-		return (a);
-	return (b);
+Fixed& Fixed::max(Fixed& a, Fixed& b) {
+	return (a > b ? a : b);
 }
 
-const Fixed& Fixed::max(const Fixed& a, const Fixed&b) {
-	if (a.toFloat() > b.toFloat())
-		return (a);
-	return (b);
+const Fixed& Fixed::max(const Fixed& a, const Fixed& b) {
+	return (a > b ? a : b);
 }
 
 bool Fixed::operator>(const Fixed& other) const {
