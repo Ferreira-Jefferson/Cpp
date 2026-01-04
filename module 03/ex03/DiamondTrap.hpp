07c97e29339a0c73c019750964fb61887e3bc1a8
Ferreira-Jefferson <jefferson.ctc.ferreira@outlook.com>
2026-01-04 14:13:33 -0300

    module 03 - ex03

---
+++ module 03/ex03/DiamondTrap.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "FragTrap.hpp"
+#include "ScavTrap.hpp"
+
+class DiamondTrap : virtual public FragTrap, virtual public ScavTrap {
+	private:
+		std::string _name;
+
+	public:
+		DiamondTrap();
+		DiamondTrap(std::string name);
+		DiamondTrap(const DiamondTrap& other);
+		DiamondTrap& operator=(const DiamondTrap& other);
+		~DiamondTrap();
+
+		void attack(const std::string& target);
+		void whoAmI();
+};
