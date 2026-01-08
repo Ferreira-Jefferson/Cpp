a65fcc61228a143e3b8ba23d3f0b3d9a1b00eea4
Jefferson Cristian Tertuliano Cavalcante Ferreira <jtertuli@c1r1p3.42sp.org.br>
2026-01-08 07:55:33 -0300

    refactor: add messeges required in subject

--- module 04/ex00/Cat.cpp
+++ module 04/ex00/Cat.cpp
@@ -1,21 +1,26 @@
 #include "Cat.hpp"
 
 Cat::Cat(void) {
+	std::cout << "Default constructor called" << std::endl;
 	type = "Cat";
 }
 
 Cat::Cat(const Cat& other) {
+	std::cout << "Copy constructor called" << std::endl;
 	*this = other;
 }
 
 Cat& Cat::operator=(const Cat& other) {
+	std::cout << "Copy assignment operator called" << std::endl;
 	if (this == &other)
 		return (*this);
 	type = other.type;
 	return (*this);
 }
 
-Cat::~Cat(void) {}
+Cat::~Cat(void) {
+	std::cout << "Destructor called" << std::endl;
+}
 
 
 void Cat::makeSound(void) const {
