4ab1f14dc25117cf9f30e7285d0dd8237107ece0
Jefferson Cristian Tertuliano Cavalcante Ferreira <jtertuli@c1r1p3.42sp.org.br>
2026-01-08 07:51:18 -0300

    refactor: add messeges required in subject

--- module 03/ex02/FragTrap.cpp
+++ module 03/ex02/FragTrap.cpp
@@ -21,10 +21,10 @@ FragTrap::FragTrap(const FragTrap& other) : ClapTrap(other) {
 }
 
 FragTrap& FragTrap::operator=(const FragTrap& other) {
+	std::cout << "FragTrap assignment operator called" << std::endl; 
 	if (this != &other) {
 		ClapTrap::operator=(other);
 	}
-	std::cout << "FragTrap assignment operator called" << std::endl;
 	return *this;
 }
 
