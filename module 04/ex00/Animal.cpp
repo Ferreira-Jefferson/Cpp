26a7dc46972b4387bff48d496093a3276b4dff6b
Jefferson Cristian Tertuliano Cavalcante Ferreira <jtertuli@c1r1p3.42sp.org.br>
2026-01-08 07:55:33 -0300

    refactor: add messeges required in subject

--- module 04/ex00/Animal.cpp
+++ module 04/ex00/Animal.cpp
@@ -1,19 +1,25 @@
 #include "Animal.hpp"
 
-Animal::Animal(void) {}
+Animal::Animal(void) {
+	std::cout << "Default constructor called" << std::endl;
+}
 
 Animal::Animal(const Animal& other) {
+	std::cout << "Copy constructor called" << std::endl;
 	*this = other;
 }
 
 Animal& Animal::operator=(const Animal& other) {
+	std::cout << "Copy assignment operator called" << std::endl;
 	if (this == &other)
 		return (*this);
 	type = other.type;
 	return (*this);
 }
 
-Animal::~Animal(void) {}
+Animal::~Animal(void) {
+	std::cout << "Destructor called" << std::endl;
+}
 
 std::string Animal::getType(void) const {
 	return (type);
