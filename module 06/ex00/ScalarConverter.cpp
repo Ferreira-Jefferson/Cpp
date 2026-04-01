44603673278ec1438eef40a068184f5097d98253
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2026-04-01 17:50:42 -0300

    ex00

---
+++ module 06/ex00/ScalarConverter.cpp
@@ -0,0 +1,48 @@
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter(void) {}
+
+ScalarConverter::ScalarConverter(const ScalarConverter& copy) {}
+
+ScalarConverter &ScalarConverter::operator=(const ScalarConverter& obj) {
+    return *this;
+}
+
+ScalarConverter::~ScalarConverter(void) {}
+
+void ScalarConverter::convert(std::string literal) {
+    std::string special[] = {"nan", "nanf", "+inf", "-inf", "+inff", "-inff"};
+
+    for (int i=0; i < sizeof(special) / sizeof(special[0]); i++)
+    {
+        if (literal.compare(special[i]))
+        {
+            std::cout << "char: " << special[i] << std::endl;
+            std::cout << "int: " << special[i] << std::endl;
+            std::cout << "float: " << special[i] + "f" << std::endl;
+            std::cout << "double: " << special[i] << std::endl;
+            return ;
+        }
+    }
+    // char 
+    if (literal[0] == '\'')
+    {
+        if (literal.length() != 3)
+            std::cout << "char: " << "impossible" << std::endl;
+        else if (!isprint(literal[1]))
+            std::cout << "char: " << "not displayable" << std::endl;
+        else
+        {
+            std::cout << "char: " << literal[1] << std::endl;
+            std::cout << "int: " << static_cast<int>(literal[1]) << std::endl;
+            std::cout << "float: " << static_cast<float>(literal[1]) << std::endl;
+            std::cout << "double: " << static_cast<double>(literal[1]) << std::endl;
+        }
+        return ;
+    }
+    // float
+
+    // double 
+    // int
+
+}
---
+++ module 06/ex00/ScalarConverter.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <cctype>
+
+class ScalarConverter {
+    ScalarConverter(void);
+    ScalarConverter(const ScalarConverter& copy);
+    ScalarConverter& operator=(const ScalarConverter& copy);
+    ~ScalarConverter(void);
+    
+    public:
+        static void convert(std::string literal);    
+};
