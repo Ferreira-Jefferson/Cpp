24cb7a4ef33b542b538f041354de228fc49ab655
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2026-04-17 11:41:32 -0300

    ex01: implement iter function template

---
+++ module 07/ex01/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "iter.hpp"
+
+template <typename T>
+void print(T const &val) {
+	std::cout << val << std::endl;
+}
+
+template <typename T>
+void doubleVal(T &val) {
+	val *= 2;
+}
+
+int main(void) {
+	int arr[] = {1, 2, 3, 4, 5};
+	std::size_t len = sizeof(arr) / sizeof(arr[0]);
+
+	std::cout << "before:" << std::endl;
+	::iter(arr, len, print<int>);
+
+	::iter(arr, len, doubleVal<int>);
+
+	std::cout << "after doubling:" << std::endl;
+	::iter(arr, len, print<int>);
+
+	const std::string strs[] = {"hello", "world", "42"};
+	std::size_t slen = sizeof(strs) / sizeof(strs[0]);
+
+	std::cout << "const strings:" << std::endl;
+	::iter(strs, slen, print<std::string>);
+
+	return 0;
+}
