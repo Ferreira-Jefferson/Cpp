8f1eaa08d0ad9090648cda791a6e5d99754e2cf4
Jefferson Cristian Tertuliano Cavalcante Ferreira <jtertuli@c1r1p4.42sp.org.br>
2026-05-05 21:39:56 -0300

    add includes

--- module 07/ex02/main.cpp
+++ module 07/ex02/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib> 
+#include <ctime> 
 #include <Array.hpp>
 
 #define MAX_VAL 750
@@ -7,7 +9,6 @@ int main(int, char**)
     Array<int> numbers(MAX_VAL);
     int* mirror = new int[MAX_VAL];
     srand(time(NULL));
-
     for (int i = 0; i < MAX_VAL; i++)
     {
         const int value = rand();
@@ -21,11 +22,13 @@ int main(int, char**)
     }
 
     for (int i = 0; i < MAX_VAL; i++)
+    {
         if (mirror[i] != numbers[i])
         {
             std::cerr << "didn't save the same value!!" << std::endl;
             return 1;
         }
+    }
     try
     {
         numbers[-2] = 0;
@@ -44,8 +47,9 @@ int main(int, char**)
     }
 
     for (int i = 0; i < MAX_VAL; i++)
+    {
         numbers[i] = rand();
-    
-    delete [] mirror;
+    }
+    delete [] mirror;//
     return 0;
 }
